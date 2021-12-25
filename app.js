4092de2bb884f43a336ceceb1f42dd816740e0be
Vaibhav <aroravaibhav817@gmail.com>
2021-12-24 21:58:18 -0800

    Inital commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let Items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
+app.get("/", (req, res) => 
+{
+    const day = date.getDay();
+    res.render("list", { listTitle : day, newListItem: Items });
+});
+
+app.post("/", (req, res) => {
+  
+    const Item = req.body.newItem;
+    if(req.body.list === "Work")
+    {
+       workItems.push(Item);
+       res.redirect("/work");
+    }
+    else
+    {
+        Items.push(Item);
+        res.redirect("/");
+    }
+ 
+});
+
+app.get("/work", (req, res) =>
+{
+    const work = "Work List";
+    res.render("list", {listTitle : work, newListItem: workItems} )
+});
+
+app.listen(3000, console.log("Server started at port 3000"));
